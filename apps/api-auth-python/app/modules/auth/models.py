0fcf28b685b06a4cfd81e4da9c732f3b2ab5056d
wjddudgns <wjdqhdhfflae@naver.com>
2025-10-29 16:10:57 +0000

    1주차 구현 10/30

--- apps/api-auth-python/app/modules/auth/models.py
+++ apps/api-auth-python/app/modules/auth/models.py
@@ -1,12 +1,7 @@
 from sqlalchemy import String, Boolean, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 from app.db.base import Base
-import enum
-
-class UserRole(str, enum.Enum):
-    Author = "Author"
-    Editor = "Editor"
-    Admin = "Admin"
+from .types import UserRole
 
 class User(Base):
     __tablename__ = "users"
@@ -15,5 +10,5 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(320), unique=True, nullable=False, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.Author, nullable=False)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole, name="user_role"), default=UserRole.Author, nullable=False)
 
